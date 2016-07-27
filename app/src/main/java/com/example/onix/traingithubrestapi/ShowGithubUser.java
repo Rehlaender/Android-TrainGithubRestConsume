7555bf17b9b223f0d1a0d9c34769e4f9afd2b823
Rehlaender <mario.avila1d@gmail.com>
2016-07-27 12:19:54 -0500

    Finish the image load

--- app/src/main/java/com/example/onix/traingithubrestapi/ShowGithubUser.java
+++ app/src/main/java/com/example/onix/traingithubrestapi/ShowGithubUser.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
